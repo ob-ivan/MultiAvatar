6ef6c5e8162e759946205797bedcb1b217401714
ob-ivan <ob-ivan@github.com>
2012-08-13 23:20:26 +0400

    Rewrite to OOP-styled script

--- source/MultiAvatar.php
+++ source/MultiAvatar.php
@@ -1,64 +1,191 @@
 ﻿<?php
-define ('KEY_FILENAME',  0);
-define ('KEY_EXTENSION', 1);
 
-if (! is_readable ($dn = dirname(__FILE__) . '/' . ($subdn = 'avatars')))
+// config //
+
+define ('DIR_AVATARS', 'avatars');
+define ('CACHE_FILENAME', '.filelist');
+
+// error handling //
+
+function exception_error_handler ($errno, $errstr, $errfile, $errline)
 {
-    print 'Directory "' . $subdn . '" not found!';
-    die;
+    throw new ErrorException ($errstr, $errno, 0, $errfile, $errline);
 }
-$subflfn = '.filelist';
-$flfn = $dn . '/' . $subflfn;
-$reset = array_key_exists ('reset_filelist', $_POST) && $_POST['reset_filelist'] == 1;
-$files = false;
-if (is_readable ($flfn) || ! $reset)
+set_error_handler('exception_error_handler');
+
+// Entry //
+
+interface EntryInterface
 {
-    $files = @unserialize (@file_get_contents ($flfn));
+    public function __construct ($filepath, $extension);
+    public function printHeaders ();
+    public function printBody ();
 }
-if (empty ($files) || $reset)
+
+class Entry implements EntryInterface
 {
-    $dir = dir ($dn);
-    $files = array ();
-    $bad = array ('.', '..', $subflfn);
-    $good = array ('png', 'jpg', 'gif');
-    while (true)
+    // const //
+    
+    static protected $ACCEPT_EXTENSIONS = array ('png', 'jpg', 'jpeg', 'gif');
+    
+    // var //
+    
+    protected $filepath;
+    protected $extension;
+    
+    // public //
+    
+    public function __construct ($filepath, $extension)
     {
-        $f = $dir->read();
-        if ($f === false)
+        if (! file_exists ($filepath))
         {
-            break;
+            throw new EntryException ('File ' . $filepath . ' does not exist', EntryException::FILE_DOES_NOT_EXIST);
         }
         
-        if (in_array ($f, $bad))
+        if (! in_array ($extension, self::$ACCEPT_EXTENSIONS))
         {
-            continue;
+            throw new EntryException ('Extension ' . $extension . ' is not accepted', EntryException::EXTENSION_IS_NOT_ACCEPTED);
         }
-        $ext = preg_replace ('/^.*\.([a-z]{3,4})$/i', '$1', $f);
-        if (! in_array ($ext, $good))
-        {
-            continue;
-        }
-        $files[] = array (KEY_FILENAME => $f, KEY_EXTENSION => $ext == 'jpg' ? 'jpeg' : $ext);
+        
+        $this->filepath = $filepath;
+        $this->extension = $extension === 'jpg' ? 'jpeg' : $extension;
     }
-    file_put_contents ($flfn, serialize ($files));
+    
+    public function printHeaders ()
+    {
+        header ('Content-Transfer-Encoding: binary');
+        header ('Content-Type: image/' . $this->extension);
+        header ('Content-Length: ' . filesize ($this->filepath));
+    }
+    
+    public function printBody ()
+    {
+        ob_clean();
+        flush();
+        readfile ($this->filepath);
+    }
 }
-if ($reset)
-{
-    header ('Location: ' . $_SERVER['PHP_SELF']);
-    die;
-}
-if (empty ($files))
-{
-    print 'Directory "' . $subdn . '" is empty!';
-    die;
-}
-shuffle ($files);
-$entry = array_pop($files);
 
-header ('Content-Transfer-Encoding: binary');
-header ('Content-Type: image/' . $entry[KEY_EXTENSION]);
-header ('Content-Length: ' . filesize($dn . '/' . $entry[KEY_FILENAME]));
-ob_clean();
-flush();
-readfile ($dn . '/' . $entry[KEY_FILENAME]);
+class EntryException extends Exception
+{
+    const FILE_DOES_NOT_EXIST       = __LINE__;
+    const EXTENSION_IS_NOT_ACCEPTED = __LINE__;
+}
 
+// MultiAvatar //
+
+interface MultiAvatarInterface
+{
+    public function __construct  ($dirName, $cacheFilename);
+    public function getEntry ();
+}
+
+class MultiAvatar implements MultiAvatarInterface
+{
+    // var //
+    
+    protected $dirPath;
+    protected $cacheFilename;
+    protected $cacheFilepath;
+    protected $isReset = false;
+    
+    // public //
+    
+    public function __construct  ($dirName, $cacheFilename)
+    {
+        $this->dirPath = dirname(__FILE__) . '/' . $dirName;
+        $this->cacheFilename = $cacheFilename;
+        $this->cacheFilepath = $this->dirPath . '/' . $cacheFilepath;
+        $this->isReset = isset ($_POST['reset_filelist']) && intval ($_POST['reset_filelist']) === 1;
+        
+        if (! is_readable ($this->dirPath))
+        {
+            throw new MultiAvatarException ('Directory ' . $this->dirPath . ' is not readable', MultiAvatarException::DIRECTORY_IS_NOT_READABLE);
+        }
+        
+        if ($this->isReset)
+        {
+            $this->resetCache();
+        }
+    }
+    
+    public function getEntry ()
+    {
+        $entries = $this->getEntries();
+        if (empty ($entries))
+        {
+            $this->resetCache();
+            $entries = $this->getEntries();
+            if (empty ($entries))
+            {
+                throw new MultiAvatarException ('Entry list is empty', MultiAvatarException::ENTRY_LIST_IS_EMPTY);
+            }
+        }
+        shuffle ($entries);
+        return array_shift ($entries);
+    }
+    
+    // protected //
+    
+    protected function getEntries ()
+    {
+        return unserialize (file_get_contents ($this->cacheFilepath));
+    }
+    
+    protected function resetCache ()
+    {
+        $dir = dir ($this->dirPath);
+        $entries = array ();
+        $bad = array ('.', '..', $this->cacheFilename);
+        while (true)
+        {
+            $filename = $dir->read();
+            if ($filename === false)
+            {
+                break;
+            }
+            
+            if (in_array ($filename, $bad))
+            {
+                continue;
+            }
+            $extension = preg_replace ('/^.*\.([a-z]{3,4})$/i', '$1', $filename);
+            try
+            {
+                $entries[] = new Entry ($filename, $extension);
+            }
+            catch (EntryException $e)
+            {
+                // TODO: Write to log.
+            }
+        }
+        $dir->close();
+        if (empty ($entries))
+        {
+            throw new MultiAvatarException ('Directory ' . $this->dirPath . ' is empty', MultiAvatarException::DIRECTORY_IS_EMPTY);
+        }
+        file_put_contents ($this->cacheFilepath, serialize ($entries));
+    }
+}
+
+class MultiAvatarException extends Exception
+{
+    const DIRECTORY_IS_NOT_READABLE = __LINE__;
+    const DIRECTORY_IS_EMPTY        = __LINE__;
+    const ENTRY_LIST_IS_EMPTY       = __LINE__;
+}
+
+// body //
+
+try
+{
+    $multiAvatar = new MultiAvatar (DIR_AVATARS, CACHE_FILENAME);
+    $entry = $multiAvatar->getEntry();
+    $entry->printHeaders();
+    $entry->printBody();
+}
+catch (Exception $e)
+{
+    print get_class ($e) . ' (' . $e->getCode() . '): ' . $e->getMessage();
+}
+die;
